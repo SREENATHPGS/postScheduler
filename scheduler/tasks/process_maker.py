b74610e5daf9f9e4067693ab3aa3227fbd222094
mechanic <sreenath@shiftbytes.co.in>
2021-04-16 01:48:56 +0530

    Added loose code to start subprocesses to post apis.

--- scheduler/tasks/process_maker.py
+++ scheduler/tasks/process_maker.py
@@ -1,11 +1,17 @@
 from scheduler import celery
 from scheduler.db import get_db
 from datetime import datetime
-import multiprocessing
+import billiard as multiprocessing
 
-def schedulerProcessFn():
-    pass
-
+def schedulerProcessFn(postObject):
+    print(f'Posting......{postObject["id"]}')
+    date = datetime.strptime(postObject["date"], '%d-%m-%Y:%H:%M:%S') #12-04-2020:23:30:00
+    yetToPost = True
+    while yetToPost:
+        tDiff = int(((date - datetime.now()).total_seconds())/60)
+        if tDiff == 0:
+            print("Calling instagram apis...")
+            yetToPost = False
 
 
 
@@ -21,6 +27,7 @@ def create_processes():
         tDiff = int(((date - datetime.now()).total_seconds())/60)
         if -5 <= tDiff <= 5:
             print("placing task.")
-            schedulerProcess = multiprocessing.Process(name = "schedulerProcess", target = schedulerProcessFn)
+            schedulerProcess = multiprocessing.Process(name = "schedulerProcess", target = schedulerProcessFn, args = [post])
+            schedulerProcess.start()
 
 
